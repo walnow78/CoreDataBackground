460e1c045c95e57d5af065dd9092af4d09cdf290
Pawel Walicki <pwalicki@gmail.com>
2016-02-13 19:58:08 +0100

    Initial commit

---
+++ CoreDataBackground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataBackground
+//
+//  Created by Pawel Walicki on 13/02/16.
+//  Copyright © 2016 Pawel Walicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
